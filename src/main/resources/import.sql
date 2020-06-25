80057257ca094a7668e9e29474a1fc73b7556867
Welton Machado <work.leao@gmail.com>
2020-06-25 14:53:15 -0300

    Adicionando INSERTS para serem pre-carregados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,16 @@
 --EFETUE A CRIACAO DOS SCRIPTS QUE IRAO FAZER A CARGA DOS DADOS
+
+INSERT INTO ESTUDANTE(nome, email, telefone, matricula, curso)
+VALUES('Xawoy','xawoy@tms.com.br','7777','123456','Engenharia da computação');
+
+INSERT INTO ESTUDANTE(nome, email, telefone, matricula, curso)
+VALUES('Furae','furae@tms.com.br','7778', '678912','Ciência da computação');
+
+INSERT INTO ESTUDANTE(nome, email, telefone, matricula, curso)
+VALUES('Fupuy','fupuy@tms.com.br','7779','321654','Engenharia da computação');
+
+INSERT INTO ESTUDANTE(nome, email, telefone, matricula, curso)
+VALUES('Kuer','kuer@tms.com.br','7780','654987','Ánalise de Dados');
+
+INSERT INTO ESTUDANTE(nome, email, telefone, matricula, curso)
+VALUES('Blias','blias@tms.com.br','7781','666131','Ciência da computação');
