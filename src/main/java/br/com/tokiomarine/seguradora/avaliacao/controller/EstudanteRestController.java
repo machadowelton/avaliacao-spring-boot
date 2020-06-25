453ab26ac7efb9ac8e2da2486cf98e3272921ccd
Welton Machado <work.leao@gmail.com>
2020-06-25 13:44:42 -0300

    Implementando a classe EstudanteRestController

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/controller/EstudanteRestController.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/controller/EstudanteRestController.java
@@ -1,15 +1,62 @@
 package br.com.tokiomarine.seguradora.avaliacao.controller;
 
-// TODO não esquecer de usar as anotações para criação do restcontroller
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tokiomarine.seguradora.avaliacao.entidade.Estudante;
+import br.com.tokiomarine.seguradora.avaliacao.service.EstudandeService;
+import br.com.tokiomarine.seguradora.avaliacao.service.EstudanteServiceImpl;
+
+@RestController
+@RequestMapping("/estudantes")
 public class EstudanteRestController {
 
 	// TODO caso você não conheça THEMELEAF faça a implementação dos métodos em forma de RESTCONTROLLER (seguindo o padrão RESTFUL)
 
+	private final EstudandeService estudandeService;
+	
+	public EstudanteRestController(final EstudanteServiceImpl estudanteServiceImpl) {
+		this.estudandeService = estudanteServiceImpl;
+	}
+	
 	// TODO IMPLEMENTAR CADASTRO DE ESTUDANTES (POST)
-
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void cadastrar(@Valid @RequestBody Estudante estudante) {
+		estudandeService.cadastrarEstudante(estudante);
+	}
+		
 	// TODO IMPLEMENTAR ATUALIZACAO DE ESTUDANTES (PUT)
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void atualizar(
+			@PathVariable("id") Long id,
+			@Valid @RequestBody Estudante estudante) {
+		estudandeService.atualizarEstudante(estudante);
+	}
 
 	// TODO IMPLEMENTAR A LISTAGEM DE ESTUDANTES (GET)
+	@GetMapping
+	public List<Estudante> listar() {
+		return estudandeService.buscarEstudantes();
+	}
 
 	// TODO IMPLEMENTAR A EXCLUSÃO DE ESTUDANTES (DELETE)
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removerPorId(@PathVariable("id") Long id) {
+		estudandeService.removerEstudantePorId(id);
+	}
 }
