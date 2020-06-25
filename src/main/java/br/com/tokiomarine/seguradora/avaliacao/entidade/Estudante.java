eee3b3ce2a3dcfcd925e3082788abe987804fef4
Welton Machado <work.leao@gmail.com>
2020-06-25 13:27:35 -0300

    2 - Modelando a entidade Estudante

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -1,5 +1,123 @@
 package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Estudante {
-	// TODO Implementar a entidade Estudante conforme solicitado
+	
+	@Id
+	@GeneratedValue(
+	    strategy= GenerationType.AUTO, 
+	    generator="native"
+	)
+	@GenericGenerator(
+	    name = "native", 
+	    strategy = "native"
+	)
+	private Long id;
+	
+	@NotNull(message = "Nome é obrigatório")
+	private String nome;
+	
+	@NotNull(message = "Email é obrigatório")
+	private String email;
+	
+	private String telefone;
+	
+	public Estudante() { }
+
+	public Estudante(Long id, String nome, String email, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object estudante) {
+		if (this == estudante)
+			return true;
+		if (estudante == null)
+			return false;
+		if (getClass() != estudante.getClass())
+			return false;
+		Estudante other = (Estudante) estudante;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudante(id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ")";
+	}	
+	
 }
+
